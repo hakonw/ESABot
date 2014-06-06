18324d1d07a2b3663fcbf732a2f82817dd91ee57
hakonw <hakonwar@gmail.com>
2014-06-06 16:52:21 +0200

    Lastes update, more grammar fix #swag

---
+++ src/org/no_ip/kermi/rpg/RpgCommand.java
@@ -0,0 +1,105 @@
+package org.no_ip.kermi.rpg;
+
+import java.util.HashMap;
+
+import org.no_ip.kermi.rpg.RpgUtil;
+
+import net.aslettemark.esabot.ESABot;
+import net.aslettemark.esabot.command.CommandExecutor;
+
+public class RpgCommand extends CommandExecutor {
+
+    public RpgUtil util;
+    public int[][] rpgID;
+    public HashMap<String, Integer> hmID;
+    public String[] rpgIDS;
+
+    public RpgCommand(ESABot bot) {
+        super(bot);
+        this.util = new RpgUtil(bot, this);
+        this.rpgID = new int[10][5];
+        this.hmID = new HashMap<String, Integer>();
+        this.rpgIDS = new String[10];
+    }
+
+    @Override
+    public void execute(String channel, String sender, String login, String hostname, String command, boolean pm) {
+
+        String[] commandArgu = command.split(" ");
+        switch (commandArgu[1]) {
+            case "help":
+                util.makeRpgHelp(channel);
+                break;
+            case "attack":
+                if (rpgID[hmID.get(sender)][0] == 1) {
+                    switch (commandArgu[3]) {
+                        case "fist":
+                            util.makeRpgAttack(sender, commandArgu[2], commandArgu[3], 2, 1, channel, false);
+                            break;
+                        case "fireball":
+                            util.makeRpgAttack(sender, commandArgu[2], commandArgu[3], 6, 3, channel, false);
+                            break;
+                        case "dagger":
+                            util.makeRpgAttack(sender, commandArgu[2], commandArgu[3], 3, 1, channel, false);
+                            break;
+                        case "heal":
+                            util.makeRpgAttack(sender, commandArgu[2], commandArgu[3], -6, 2, channel, true);
+                            break;
+                        case "death": //will be removed
+                            util.makeRpgAttack(sender, commandArgu[2], commandArgu[3], 30, 0, channel, false);
+                            break;
+                        default:
+                            bot.sendMessage(channel, "That aint an attack.");
+                    }
+                } else {
+                    bot.sendMessage(channel, "It looks like you're dead");
+                }
+                break;
+            case "init":
+                util.makeRpgReg(sender, channel);
+                break;
+            case "walk":
+                util.makeRpgWalk();
+                bot.sendMessage(channel, "Walk is not implemented yet");
+                break;
+            case "info":
+                if (commandArgu[2].isEmpty()) {
+                    util.makeRpgCheatValues(sender, channel);
+                } else {
+                    util.makeRpgCheatValues(commandArgu[2], channel);
+                }
+                break;
+            case "cheat":
+                switch (commandArgu[2]) {
+                    case "reset":
+                        if (commandArgu[3].isEmpty()) {
+                            bot.sendMessage(channel, "Syntax error use <.rpg cheat reset 'name'>");
+                        } else {
+                            util.makeRpgRegVal(commandArgu[3], channel);
+                            bot.sendMessage(channel, "Done with reset to " + commandArgu[3]);
+                        }
+                        break;
+                    case "portal":
+                        bot.sendMessage(channel, "I'm doing Science and I'm still alive.");
+                        bot.sendMessage(channel, "I feel FANTASTIC and I'm still alive.");
+                        bot.sendMessage(channel, "While you're dying I'll be still alive.");
+                        bot.sendMessage(channel, "And when you're dead I will be still alive.");
+                        bot.sendMessage(channel, "STILL ALIVE");
+                        bot.sendMessage(channel, "STILL ALIVE");
+                        break;
+                    case "update_all":
+                        util.makeRpgUpdateAll(channel);
+                        break;
+                    case "update":
+                        util.makeRpgUpdate(commandArgu[3], channel);
+                        break;
+                    default:
+                        bot.sendMessage(channel, "That is not a cheat.");
+                }
+                break;
+
+            default:
+                bot.sendMessage(channel, "Unknown command, you could try '.rpg help'.");
+        }
+    }
+}
---
+++ src/org/no_ip/kermi/rpg/RpgUtil.java
@@ -0,0 +1,98 @@
+package org.no_ip.kermi.rpg;
+
+import net.aslettemark.esabot.ESABot;
+
+public class RpgUtil {
+
+    protected ESABot bot;
+    public RpgCommand cmd;
+
+    public RpgUtil(ESABot bot, RpgCommand c) {
+        this.bot = bot;
+        this.cmd = c;
+    }
+
+    public void makeRpgUpdateAll(String channel) {
+        for (int i = 0; i <= cmd.hmID.size(); i++) {
+            int tempLiv = cmd.rpgID[i][1];
+            if (tempLiv <= 0) {
+                cmd.rpgID[i][0] = 0;
+                bot.sendMessage(channel, cmd.rpgIDS[i] + " is dead. RIP IN PEACE!");
+            }
+        }
+    }
+
+    public void makeRpgUpdate(String sender, String channel) {
+        if (cmd.rpgID[cmd.hmID.get(sender)][1] <= 0) {
+            cmd.rpgID[cmd.hmID.get(sender)][0] = 0;
+            bot.sendMessage(channel, sender + " is dead. RIP IN PEACE!");
+        }
+    }
+
+    public void makeRpgAttack(String sender, String receiver, String spell, int damage, int stamina, String channel, Boolean heal) {
+        int tempRpgId = cmd.hmID.get(sender);
+        if (!(cmd.hmID.get(sender) == null)) {
+            if (!(cmd.hmID.get(receiver) == null)) {
+                if (cmd.rpgID[tempRpgId][2] >= stamina) {
+                    if (cmd.rpgID[cmd.hmID.get(receiver)][1] >= 0) {
+                        final int tempRpgStamina = cmd.rpgID[tempRpgId][2];
+                        final int tempRpgHp = cmd.rpgID[tempRpgId][1];
+
+                        cmd.rpgID[tempRpgId][2] = tempRpgStamina - stamina;
+                        cmd.rpgID[tempRpgId][1] = tempRpgHp - damage;
+
+                        if (heal) {
+                            bot.sendMessage(channel, sender + " healed " + receiver + " with " + -damage + " hp with his " + spell + "ing spell and used up " + stamina + " stamina.");
+                        } else {
+                            bot.sendMessage(channel, sender + " took " + damage + " hp to " + receiver + " with his " + spell + " and used up " + stamina + " stamina.");
+                        }
+                        makeRpgUpdate(receiver, channel);
+                    } else {
+                        bot.sendMessage(channel, "That dude is dead, try attacking another dude.");
+                    }
+                } else {
+                    bot.sendMessage(channel, "It looks like you're out of stamina " + cmd.rpgID[tempRpgId][2] + "/" + stamina);
+                }
+            } else {
+                bot.sendMessage(channel, receiver + " ain't a guy.");
+            }
+        } else {
+            bot.sendMessage(channel, "You're not registered, use '.rpg init'.");
+        }
+    }
+
+    public void makeRpgReg(String sender, String channel) {
+        if (cmd.hmID.get(sender) == null) {
+            cmd.hmID.put(sender, new Integer(cmd.hmID.size()));
+            makeRpgRegVal(sender, channel);
+            bot.sendMessage(channel, "You are now signed up with ID " + cmd.hmID.get(sender));
+        } else {
+            bot.sendMessage(channel, "You're registered with ID " + cmd.hmID.get(sender));
+        }
+    }
+
+    public void makeRpgRegVal(String sender, String channel) {
+        int tempIdVal = cmd.hmID.get(sender);
+        cmd.rpgIDS[tempIdVal] = sender;
+        cmd.rpgID[tempIdVal][0] = 1; // liv
+        cmd.rpgID[tempIdVal][1] = 20; // hp
+        cmd.rpgID[tempIdVal][2] = 12; // stamina
+    }
+
+    public void makeRpgCheatValues(String sender, String channel) {
+        int tempValue[] = new int[3];
+        for (int i = 0; i <= 2; i++) {
+            tempValue[i] = cmd.rpgID[cmd.hmID.get(sender)][i];
+        }
+        bot.sendMessage(channel, "Alive = " + tempValue[0] + ", hp = " + tempValue[1] + ", stamina = " + tempValue[2] + ".");
+    }
+
+    public void makeRpgHelp(String channel) {
+        bot.sendMessage(channel, "I should implement help.");
+    }
+
+    public void makeRpgWalk() {
+
+    }
+
+}
